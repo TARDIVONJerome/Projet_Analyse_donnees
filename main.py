df0c1c0bccfaf4ae18f8819293ff44b4ba0fe9a7
TARDIVON JEROME p2411304 <jerome.tardivon@etu.univ-lyon1.fr>
2025-05-09 13:54:52 +0000

    Update file main.py

--- main.py
+++ main.py
@@ -52,22 +52,22 @@ readFile("FD_SALAAN_2016.csv")
 
 
 
-def salaryManWoman(workers):
+def salaryManWoman(workers): #calcule la répartition des hommes et de femmes dans les différentes tranches de salaire
     repM=[]
     repF=[]
     
-
     for i in range(0,24):
         repM.append(0)
         repF.append(0)
 
     for i in range (0,len(workers)):
-        if(workers[i].sex=="1"):
-            repM[int(workers[i].salary)]+=1
-        else:
-            repF[int(workers[i].salary)]+=1
+        if(workers[i].age!='' and int(workers[i].age)>15 and int(workers[i].age)<100): #on ne prend en compte que les salariés entre 16 et 99 ans et on ignore également ceux ayant un age non renseigné à cause de données aberrantes (salariés en CDI de 0, 1 ou meme 127 ans)
+            if(workers[i].sex=="1"):    #1=homme 2=femme
+                repM[int(workers[i].salary)]+=1
+            else:
+                repF[int(workers[i].salary)]+=1
 
-    print(repM)
+    print(repM) #affichage
     print(repF)
 
 # calcul des effectifs d'homme et de femme dans chaque secteur d'industrie selon la nomenclature agrégée A6 
@@ -177,34 +177,33 @@ def ageSelonReg(workers):
 salaryManWoman(workers)
 
 
-def salaryOldYoung(workers):
+def salaryOldYoung(workers): # calcul la répartition des ages dans les différentes tranches de salaire
     repA=[]
 
 
-    for i in range(0,16):
+    for i in range(0,16):   #les 16 primières lignes (0 à 15 ans) sont remplies de '#' pour garder la position des indices (indice 50 = age 50)
         repA.append([])
-        for j in range (0,24):
+        for j in range (0,24): #24 trhanges de salaire (0 à 23)
             repA[i].append('#')
-    for i in range (16,100):
+    for i in range (16,100): # on vas jusqu'à 99 ans
         repA.append([])
         for j in range (0,24):
-            repA[i].append(0)
+            repA[i].append(0) # initialisation à 0
     
     for i in range (0,len(workers)):
-        if(workers[i].age!='' and int(workers[i].age)>15 and int(workers[i].age)<100): #on ne prend en compte que les salariés de 16 ans ou plus et on ignore également ceux ayant un age non renseigné à cause de données aberrantes (salariés en CDI de 0 ou 1 an)
+        if(workers[i].age!='' and int(workers[i].age)>15 and int(workers[i].age)<100): #on ne prend en compte que les salariés entre 16 et 99 ans et on ignore également ceux ayant un age non renseigné à cause de données aberrantes (salariés en CDI de 0, 1 ou meme 127 ans)
             repA[int(workers[i].age)][int(workers[i].salary)]+=1
     
-    for i in range (len(repA)):
+    for i in range (len(repA)): #affichage
         print(repA[i])
 
     return repA
+    
+repA=salaryOldYoung(workers)
 
-
-
-
-def medSalaryOldYoung(repA,varmodfilename):
-    ECC=[]
-    for i in range(0,16):
+def medSalaryOldYoung(repA,varmodfilename): # calcul les salaires médians pour chaque age (16 à 99) (besoins d'un fichier varmod pour connaitre les différentes tranches de slalaire grâce à la fonction getSalariesMinMax())
+    ECC=[] # calcul de l'effectif cumulé croissant
+    for i in range(0,16): 
         ECC.append([])
         for j in range(0,len(repA[i])):
             ECC[i].append('#')
@@ -216,95 +215,145 @@ def medSalaryOldYoung(repA,varmodfilename):
             sum+=repA[i][j]
             ECC[i].append(sum)
 
-    for i in range (len(repA)):
+    for i in range (len(repA)): #affichage
         print(ECC[i])
 
-    nummed=[]
+    nummed=[] # calcul des position des médianes
     for i in range(0,16):
         nummed.append('#')
     for i in range(16,len(ECC)):
         nummed.append((ECC[i][len(ECC[i])-1]/2))
 
-    print(nummed)
+    print(nummed) # affichage
 
     med=[]
-    salaries=getSalariesMinMax(varmodfilename)
+    salaries=getSalariesMinMax(varmodfilename) # récupération des tranches de salaires via le fichier varmod
 
     for i in range (0,16):
         med.append('#')
-    for i in range (16,len(nummed)):
-        # if(nummed[i]%1==0):
+    for i in range (16,len(nummed)): #calcul des médianes
+        
             
-            for j in range (0,len(ECC[i])):
-                if(nummed[i]-ECC[i][j]<=0):
+        for j in range (0,len(ECC[i])):
+            if(nummed[i]-ECC[i][j]<=0): # vérification de la présence de la médiane dans la tranche actuelle
                     
                     
-                    med.append(    salaries[j][0]+    (    (nummed[i]-ECC[i][j-1])    *    (salaries[j][2]-salaries[j][0])    )    /    (ECC[i][j]-ECC[i][j-1])    )
-                    break
+                med.append(    salaries[j][0]+    (    (nummed[i]-ECC[i][j-1])    *    (salaries[j][2]-salaries[j][0])    )    /    (ECC[i][j]-ECC[i][j-1])    ) # formule de la médiane sur un caractère quantitatif continu
+                break
     print(med)
 
 
 
 
-
-def getSalariesMinMax(varmodfilename):
+def getSalariesMinMax(varmodfilename): # transformation des codes de tranche de salaires (0 à 23) en listes de 3 éléments : [0]-->min de la tranche ; [1]--> moyenne de la tranche ; [2]--> max de la tranche
     res=[]
     f=open(varmodfilename)
     lines=f.readlines()
     cpt=0
-    for i in range (0,len(lines)):
+    for i in range (0,len(lines)): # récupération du nombre de tranches (au cas où les tranches ne seraies pas dans l'ordre)
         tmp=lines[i].split(';')
         if(tmp[0]=="TRNNETO"):
             cpt+=1
 
 
     for i in range (0,cpt):
-        res.append([None,None,None])
+        res.append([None,None,None]) #initialisation à None
 
     for i in range (0,len(lines)):
         tmp=lines[i].split(';')
         
         if(tmp[0]=="TRNNETO"):
             tmp[2]=int(tmp[2])
-            if("Moins" in tmp[3]):
-                res[tmp[2]][0]=0
-                tmpl=tmp[3].split(' ')
-                if(len(tmpl)==4):
+            if("Moins" in tmp[3]):  #vérification du cas particulier "Moins de * euros"
+                res[tmp[2]][0]=0  # min à 0
+                tmpl=tmp[3].split(' ')  #ligne temporaire
+                if(len(tmpl)==4): # gestion des cas de tailles des nombres (200 créé 1 string et 2 000 en créé 2 lors du split)
                     res[tmp[2]][2]=int(tmpl[2])
                 else:
                     res[tmp[2]][2]=int(tmpl[2]+tmpl[3])
                 
-                res[tmp[2]][1]=(res[tmp[2]][0]+res[tmp[2]][2])/2
+                res[tmp[2]][1]=(res[tmp[2]][0]+res[tmp[2]][2])/2 #moyenne de la tranche
 
-            elif("et plus" in tmp[3]):
+            elif("et plus" in tmp[3]): #vérification du cas particulier " * euros et plus"
                 tmpl=tmp[3].split(' ')
                 if(len(tmpl)==4):
                     res[tmp[2]][0]=int(tmpl[0])
                 else:
                     res[tmp[2]][0]=int(tmpl[0]+tmpl[1])
+
+                    # les valeurs max et moyenne sont laissées à None
                 
 
             else :
 
                 tmpl=tmp[3].split(' ')
 
-                if(len(tmpl)==4):
+                if(len(tmpl)==4): # vérification des cas de taille de nombre (200 et 300)
                     res[tmp[2]][2]=int(tmpl[2])
                     res[tmp[2]][0]=int(tmpl[0])
-                elif(len(tmpl)==5):
+                elif(len(tmpl)==5):  # vérification des cas de taille de nombre (900 et 1 000)
                     res[tmp[2]][2]=int(tmpl[2]+tmpl[3])
                     res[tmp[2]][0]=int(tmpl[0])
-                elif(len(tmpl)==6):
+                elif(len(tmpl)==6):  # vérification des cas de taille de nombre (2 000 et 3 000)
                     res[tmp[2]][2]=int(tmpl[3]+tmpl[4])
                     res[tmp[2]][0]=int(tmpl[0]+tmpl[1])
             
                 res[tmp[2]][1]=(res[tmp[2]][0]+res[tmp[2]][2])/2
 
+
+#////J'aurais pu mieux traiter les tailles des nombres pour ne pas être limité à 99 999 en utilisant plus de split et en bouclant la concaténation des parties du nombre ce qui aurais rendu le programme plus adaptatif mais j'ai préféré utiliser ce temps pour d'autres fonctions (car pas réellement utile ici)\\\\#
+
     
-    for i in range (0,len(res)):
+    for i in range (0,len(res)): # affichage
         print(res[i])
     
     return res
+                
+
+medSalaryOldYoung(repA,'Varmod_SALAAN_2016.csv')
+
+
+
+def medSalaryOldYoung(repA,varmodfilename): # calcul les salaires médians pour chaque age (16 à 99) (besoins d'un fichier varmod pour connaitre les différentes tranches de slalaire grâce à la fonction getSalariesMinMax())
+    ECC=[] # calcul de l'effectif cumulé croissant
+    for i in range(0,16): 
+        ECC.append([])
+        for j in range(0,len(repA[i])):
+            ECC[i].append('#')
+
+    for i in range(16,len(repA)):
+        ECC.append([])
+        sum=0
+        for j in range (0,len(repA[i])):
+            sum+=repA[i][j]
+            ECC[i].append(sum)
+
+    for i in range (len(repA)): #affichage
+        print(ECC[i])
+
+    nummed=[] # calcul des position des médianes
+    for i in range(0,16):
+        nummed.append('#')
+    for i in range(16,len(ECC)):
+        nummed.append((ECC[i][len(ECC[i])-1]/2))
+
+    print(nummed) # affichage
+
+    med=[]
+    salaries=getSalariesMinMax(varmodfilename) # récupération des tranches de salaires via le fichier varmod
+
+    for i in range (0,16):
+        med.append('#')
+    for i in range (16,len(nummed)): #calcul des médianes
+        
+            
+        for j in range (0,len(ECC[i])):
+            if(nummed[i]-ECC[i][j]<=0): # vérification de la présence de la médiane dans la tranche actuelle
+                    
+                    
+                med.append(    salaries[j][0]+    (    (nummed[i]-ECC[i][j-1])    *    (salaries[j][2]-salaries[j][0])    )    /    (ECC[i][j]-ECC[i][j-1])    ) # formule de la médiane sur un caractère quantitatif continu
+                break
+    print(med)
 
 
 # calcule la médiane des salaires selon le département
